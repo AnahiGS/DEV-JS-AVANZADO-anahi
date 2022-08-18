9c125e80e5bc66e4c16b4434ce28d8ed3caddddd
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-08-18 18:19:18 -0500

    feat: actualizado con ejemplo de npm

---
+++ my-js-avanzado/1-node/1.6-media-server/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((request, response) =>{
+    const status=200; //OK
+    const mimeType=  { 'Content-type' : 'text/html'}
+    response.writeHead(status, mimeType);
+    response.write('<html> <body> Hola Anahí </body></html>');
+    response.end();
+
+})
+
+server.listen(8000); //en la web se puede cambiar localhost:8000 por 127.0.0.1:8000
+console.log('el servidor fue iniciado...');
