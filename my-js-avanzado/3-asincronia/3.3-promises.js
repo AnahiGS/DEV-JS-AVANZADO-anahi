4eeabe13e90f7133db49e39f46cfeeed1550f453
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-08-19 18:13:46 -0500

    feat: carpetas de asincronia

--- my-js-avanzado/3-asincronia/3.3-promises.js
+++ my-js-avanzado/3-asincronia/3.3-promises.js
@@ -3,4 +3,26 @@ console.log('=== BLOQUE 4 ===');
  * BLOQUE 4- Promises (pending, reject, resolve)
  */
 
-console.log('1 pres')
+ const validatingPaymentPromise = () => {
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            const statusArray = [200, 500];
+            console.log('2 Validar la tarjeta de credito/debito y mostrar un loader');
+            const randomStatus = statusArray [Math.floor(Math.random() * statusArray.lenght)];
+            if(randomStatus === 200){
+                resolve('todo gucci');
+            }else if(randomStatus === 500) {
+                reject('tu tarjeta fue declinada :c');
+            }
+            
+        }, 0);
+    });
+};
+
+console.log('1 Presionar el boton comprar');
+validatingPaymentPromise()
+    .then(response =>{
+        console.log ('3 Ver el mensaje de gracias por su compra joven', response);
+    }).then(() => console.log ('4 Entregaremos en N fecha'))
+    .catch (error => console.log ('error:', error))
+    .finally(() => console.log ('Bloque finalizado'))
