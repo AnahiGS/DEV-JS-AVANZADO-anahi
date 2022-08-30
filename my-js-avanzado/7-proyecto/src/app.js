09977c732989ff56931a7a6d70c08b10c015683a
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-08-30 13:58:33 -0500

    fix: sólo probé cambiar el puerto

--- my-js-avanzado/7-proyecto/src/app.js
+++ my-js-avanzado/7-proyecto/src/app.js
@@ -5,12 +5,14 @@ import infoServicesRoutes from "./routes/InfoServices.routes";
 
 const app = express();
 
-app.set("port",3000);
+app.set("port",8000);
 
 
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api-restful/infoServices",infoServicesRoutes);
+
+
 
 export default app;
